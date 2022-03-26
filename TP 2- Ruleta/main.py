8f8a4191b30c0318668f822a6e184da99fe283b8
gturconi <35380017+gturconi@users.noreply.github.com>
2022-03-26 18:10:24 -0300

    Update

--- TP 2- Ruleta/main.py
+++ TP 2- Ruleta/main.py
@@ -1,21 +1,13 @@
-
-
 import random as ran
 import statistics
 import numpy as np
 import matplotlib.pyplot as plt
 
-# tipos_apuesta = {0:"Rojo",1:"Negro",2:"Par",3:"Impar",4:"Pasa",5:"Falta",6:"Docena 1",7:"Docena 2",
-#                 8:"Docena 3",9:"Columna 1",10:"Columna 2",11:"Columna 3",12:"Docenas 1 y 2",13:"Docenas 2 y 3",
-#                14:"Columnas 1 y 2",15:"Columnas 2 y 3",16:"Seisena 1",17:"Seisena 2",18:"Seisena 3",
-#                19:"Seisena 4",20:"Seisena 5",21:"Seisena 6",22:"Seisena 7",23:"Seisena 8",24:"Seisena 9",
-#                25:"Seisena 10",26:"Seisena 11",}
-
 
 if __name__ == "__main__":
     rojos = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
     negros = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
-    variables = (50,1,0)
+    variables = (50, 1, 0)
 
 
     def constante(n, len):
@@ -32,21 +24,22 @@ if __name__ == "__main__":
         plt.ylabel(msg2)
         plt.show()
 
-    def graficar2(list,msg1, msg2):
+
+    def graficar2(list, msg1, msg2):
         length = len(list)
         y_pos = np.arange(length)
-        plt.bar(y_pos, list, color="b", width = 0.25)
+        plt.bar(y_pos, list, color="b", width=0.25)
 
         list2 = []
         for i in range(length):
-            list2.append(i+1)
+            list2.append(i + 1)
         plt.xticks(y_pos, list2)
         plt.ylabel(msg1)
         plt.title(msg2)
         plt.show()
 
 
-    def martingala(valor, caja, apuesta,aciertos):
+    def martingala(valor, caja, apuesta, aciertos):
         if (valor in negros):
             caja = caja + apuesta
             aciertos = aciertos + 1
@@ -56,6 +49,7 @@ if __name__ == "__main__":
             apuesta *= 2;
         return caja, apuesta, aciertos
 
+
     def fibonacci(valor, caja, apuesta, aciertos):
         if valor in negros:
             caja = caja + apuesta
@@ -64,34 +58,37 @@ if __name__ == "__main__":
             while (True):
                 serie.append(serie[0] + serie[1])
                 serie.pop(0)
-                if (serie[0] == apuesta) :
+                if (serie[0] == apuesta):
                     break
         else:
             caja = caja - apuesta
             serie = [0, 1]
         return caja, serie[1], aciertos
-    
-    
+
+
     def dalembert(valor, caja, apuesta, aciertos):
         if valor in negros:
             caja = caja + apuesta
             aciertos = aciertos + 1
             if (apuesta > 1):
                 apuesta = apuesta - 1
-        else: 
+        else:
             caja = caja - apuesta
             apuesta = apuesta + 1
         return caja, apuesta, aciertos
 
 
-
     frecs_total_fibo = []
     flujoCaja_fibo = []
+    frecs_total_martin = []
+    flujoCaja_martin = []
+    frecs_total_dalem = []
+    flujoCaja_dalem = []
 
     funciones = [fibonacci, martingala, dalembert]
 for i in range(5):
     for funcion in funciones:
-        variables = (50,1,0)
+        variables = (50, 1, 0)
         numerosTirada = []
         flujoCaja = []
         frecs_corridas = []
@@ -100,19 +97,31 @@ for i in range(5):
         while ((flujoCaja[j] > 0) and (j < 20)):
             tirada = ran.randint(0, 36)
             numerosTirada.append(tirada)
-            variables = funcion(tirada, variables[0], variables[1],variables[2])
+            variables = funcion(tirada, variables[0], variables[1], variables[2])
             flujoCaja.append(variables[0])
-            print("Tirada número: ", funcion, "Valor: ", tirada, "Caja: ", variables[0],"Apuesta:", variables[1] ,"Aciertos: ",variables[2], "Apuesta número", j + 1)
-            frecs_corridas.append(variables[2]/(j+1))
+            print("Tirada número: ", funcion, "Valor: ", tirada, "Caja: ", variables[0], "Apuesta:", variables[1],
+                  "Aciertos: ", variables[2], "Apuesta número", j + 1)
+            frecs_corridas.append(variables[2] / (j + 1))
             j = j + 1
         if (i == 1):
-           # graficar(flujoCaja, cte_caja, "n(numero de tiradas)", "cc(cantidad de capital)")
+            # graficar(flujoCaja, cte_caja, "n(numero de tiradas)", "cc(cantidad de capital)")
             cte_caja = constante(50, len(flujoCaja))
-           # graficar(flujoCaja, cte_caja, "n(numero de tiradas)", "cc(cantidad de capital)")
-           # graficar2(frecs_corridas,"fr(frecuencia relativa)" ,"n(numero de tiradas)")
-        if(funcion == fibonacci):
+        # graficar(flujoCaja, cte_caja, "n(numero de tiradas)", "cc(cantidad de capital)")
+        # graficar2(frecs_corridas,"fr(frecuencia relativa)" ,"n(numero de tiradas)")
+        if (funcion == fibonacci):
             flujoCaja_fibo.append(flujoCaja[j - 1])
             frecs_total_fibo.append(frecs_corridas[j - 1])
+        elif(funcion == martingala):
+            flujoCaja_martin.append(flujoCaja[j - 1])
+            frecs_total_martin.append(frecs_corridas[j - 1])
+        else:
+            flujoCaja_dalem.append(flujoCaja[j - 1])
+            frecs_total_dalem.append(frecs_corridas[j - 1])
 
-graficar(flujoCaja_fibo, constante(50,5), "n(numero de tiradas)", "cc(cantidad de capital)")
-graficar2(frecs_total_fibo,"fr(frecuencia relativa)" ,"n(numero de tiradas)")
+graficar(flujoCaja_fibo, constante(50, 5), "n(numero de tiradas)", "cc(cantidad de capital)")
+graficar2(frecs_total_fibo, "fr(frecuencia relativa)", "n(numero de tiradas)")
+graficar(flujoCaja_martin, constante(50,5), "n(numero de tiradas)", "cc(cantidad de capital)")
+graficar2(frecs_total_martin,"fr(frecuencia relativa)" ,"n(numero de tiradas)")
+graficar(flujoCaja_dalem, constante(50,5), "n(numero de tiradas)", "cc(cantidad de capital)")
+graficar2(frecs_total_dalem,"fr(frecuencia relativa)" ,"n(numero de tiradas)")
+
