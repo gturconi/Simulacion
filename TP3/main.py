5383075e277dbcb66c7ef5e0a55fab30988932fb
gturconi <35380017+gturconi@users.noreply.github.com>
2022-04-14 20:02:56 -0300

    Test de Poker agregado

--- TP3/main.py
+++ TP3/main.py
@@ -1,14 +1,16 @@
 
 
-  # semilla recomendada: 4798373
-  #Otras semillas: 1257787
+# semilla recomendada: 4798373
+#Otras semillas: 1257787
+from random import random
 
-from PIL import Image
+
 import numpy as np
 import pylab as plt
 import scipy
 from statsmodels.sandbox.stats.runs import runstest_1samp
 import math
+
 
 
 
@@ -34,11 +36,13 @@ class Randu:
         for i in range(1, self.n):
             print("Valor", x[i])
             print("Normalizacion", u[i])
-        chisq_test(u)
-        testArribaAbajo(u)
-        testCorridas(u)
-        runTest(u)
-        #bitmap(np.reshape(u, (1000, 1000)))
+        #testPoker(u)
+        #chisq_test(u)
+        #testArribaAbajo(u)
+        #testCorridas(u)
+        #runTest(u)
+        bitmap(np.reshape(u, (500, 500)))
+
 
 class Rand:
     def __init__(self, n, seed):
@@ -91,12 +95,6 @@ def randomTest():
     z = np.random.random((1000, 1000))  # Test data
     bitmap(z)
 
-def uniformTest():
-    z = []
-    for i in range(65025):
-        z.append(np.random.uniform((1000, 1000))[0])
-    bitmap(np.reshape(z, (1000, 1000)))
-
 def bitmap(values):
     plt.imshow(values, cmap='gray', interpolation='nearest')
     plt.show()
@@ -148,6 +146,43 @@ def testArribaAbajo(p):
       z = (corridas - media) / desviacion
       print("Z <=" + str(z))
 
+def pokerSuma(observado, esperado):
+  return (((observado - esperado) ** 2) / esperado)
+
+def pokerCaracteres(u):
+	distintos = 0
+	dosIguales = 0
+	tresIguales = 0
+	for i in range(len(u)):
+		c = str(u[i])
+		if(c[2] != c[3] and c[2] != c[4] and c[3] != c[4]):
+			distintos += 1
+		elif(c[2] == c[3] and c[2] == c[4] and c[3] == c[4]):
+			tresIguales += 1
+		else:
+			dosIguales += 1
+	return distintos,dosIguales,tresIguales
+
+def pokerSuma(observado,esperado):
+	return(((observado-esperado)**2)/esperado)
+
+
+def testPoker(u):
+    print("Test de Poker")
+    x = []
+    y = []
+    z = []
+    distintosEsperado = 0.72
+    dosIgualesEsperado = 0.09
+    tresIgualesEsperado = 0.01
+
+    distintos, dosIguales, tresIguales = pokerCaracteres(u)
+
+    totalDistintos = pokerSuma(distintos/(255*255), distintosEsperado)
+    totalDosIguales = pokerSuma(dosIguales/(255*255), dosIgualesEsperado)
+    totalTresIguales = pokerSuma(tresIguales/(255*255), tresIgualesEsperado)
+    x2 = totalDistintos + totalDosIguales + totalTresIguales
+    print("X2 = " + str(x2))
 
 def testCorridas(u):
 	print("Test de Corridas: ")
@@ -173,8 +208,11 @@ def testCorridas(u):
 
 
 if __name__ == "__main__":
-    #uniformTest()
-    rand = Randu(255*255,1257787)
+    rand = Randu(500*500,1257787)
     rand.randu()
+    #sq = Square(1000*1000,5678)
+    #sq.mid_square()
+    decimalFractions = " "
+    #print(array.split())
 
 
