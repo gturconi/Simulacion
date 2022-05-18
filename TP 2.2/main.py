90f992141ffc4f024d331f34f9e865f21632eb3e
franGiangiordano <frangiangiordano@gmail.com>
2022-05-18 18:30:47 -0300

    codigo listo en teoria

--- TP 2.2/main.py
+++ TP 2.2/main.py
@@ -1,14 +1,25 @@
 import random
 import matplotlib.pyplot as plt
+import numpy
+import numpy as np
 import scipy
-from numpy.random import random
+import sm as sm
+import stats
+from numpy.random import random, poisson
 from numpy import floor, log as ln, exp
 from scipy.stats import chisquare
+import collections
+import copy
+from scipy.stats import kstest
+from statsmodels.distributions.empirical_distribution import ECDF
 
 
 #En esta parte del codigo estan las distribuciones
 
 # Genenador distribucion Uniforme
+from scipy.stats._stats_py import KstestResult
+
+
 def uniform (a,b):
     r = random()
     x= a+(b-a)*r
@@ -37,6 +48,7 @@ def normal(ex,stdx):
         sum += r
     x = stdx*(sum-6.0)+ex
     return x
+
 
 # Genenador distribucion Pascal
 def pascal(k,q):
@@ -84,8 +96,20 @@ def poisson(p):
 
 #Genenador distribucion Empirica
 
-#------------esto falta----------------
-
+def empirica(sample):
+    x=[]
+    for i in range(100):
+        r = random()
+        a=0
+        z=1
+        for j in sample:
+            a+=j
+            if(r<=a):
+                break
+            else:
+                z+=1
+        x.append(z)
+    return x
 
 
 
@@ -119,8 +143,8 @@ bin_p = 0.5
 dist_binomial = []
 
 #Definir parametros de la distribucion Hipergeometrica
-hiper_tn = 500
-hiper_ns = 80
+hiper_tn = 50
+hiper_ns = 50
 hiper_p = 0.6
 dist_hipergeometrica = []
 
@@ -128,15 +152,10 @@ dist_hipergeometrica = []
 poisson_p = 10
 dist_poisson = []
 
-for i in range(500):
-    dist_uniforme.append(uniform(uni_a, uni_b))
-    dist_exponencial.append(exponential(ex_ex))
-    dist_gamma.append(gamma(gamma_k, gamma_alpha))
-    dist_normal.append(normal(nor_ex, nor_stdx))
-    dist_pascal.append(pascal(pas_k, pas_q))
-    dist_binomial.append(binomial(bin_n, bin_p))
-    dist_hipergeometrica.append(hypergeometric(hiper_tn, hiper_ns, hiper_p))
-    dist_poisson.append(poisson(poisson_p))
+#Definir parametros de la distribucion Empirica
+em_sample = [0.273,0.037,0.195,0.009,0.124,0.058,0.062,0.151,0.047,0.044]
+dist_empirica = []
+
 
 #En esta parte del codigo estan las graficas a las distribuciones
 
@@ -190,47 +209,67 @@ def graficar(n, u, e, g, p, b, h, po):
     plt.show()
 
 
-graficar(dist_normal, dist_uniforme, dist_exponencial, dist_gamma, dist_pascal, dist_binomial, dist_hipergeometrica, dist_poisson);
 
-def normalizar(list):
-    xmin = min(list)
-    xmax = max(list)
-    for i, x in enumerate(list):
-        list[i] = (x - xmin) / (xmax - xmin)
-    return list
+#graficar(dist_normal, dist_uniforme, dist_exponencial, dist_gamma, dist_pascal, dist_binomial, dist_hipergeometrica, dist_poisson);
 
-def chisq_test(list):
-    stat, p = chisquare(list)
-    return stat,p
+for j in range(10):
+    for i in range(100):
+        dist_uniforme.append(uniform(uni_a, uni_b))
+        dist_exponencial.append(exponential(ex_ex))
+        dist_gamma.append(gamma(gamma_k, gamma_alpha))
+        dist_normal.append(normal(nor_ex, nor_stdx))
+        dist_pascal.append(pascal(pas_k, pas_q))
+        dist_binomial.append(binomial(bin_n, bin_p))
+        dist_hipergeometrica.append(hypergeometric(hiper_tn, hiper_ns, hiper_p))
+        dist_poisson.append(poisson(poisson_p))
 
-print("Distribucion uniforme:", dist_uniforme)
-print("Distribucion uniforme normalizada:", normalizar(dist_uniforme))
-print("TEST CHI CUADRADO PARA UNIFORME:", chisq_test(normalizar(dist_uniforme)),"\n")
+    #test de Kolmogorov Smirnov
 
-print("Distribucion exponencial:", dist_exponencial)
-print("Distribucion exponencial normalizada:", normalizar(dist_exponencial))
-print("TEST CHI CUADRADO PARA EXPONENCIAL:", chisq_test(normalizar(dist_exponencial)),"\n")
+    p_uniforme = []
+    uniforme_gen = np.random.uniform(1, 4, 100)
+    stats, p = kstest(uniforme_gen, dist_uniforme)
+    p_uniforme.append(p)
 
-print("Distribucion gamma:", dist_gamma)
-print("Distribucion gamma normalizada:", normalizar(dist_gamma))
-print("TEST CHI CUADRADO PARA GAMMA:", chisq_test(normalizar(dist_gamma)),"\n")
+    p_exponencial= []
+    exponencial_gen = np.random.exponential(7, 100)
+    stats, p = kstest(exponencial_gen, dist_exponencial)
+    p_exponencial.append(p)
 
-print("Distribucion normal:", dist_normal)
-print("Distribucion normal normalizada:", normalizar(dist_normal))
-print("TEST CHI CUADRADO PARA NORMAL:", chisq_test(normalizar(dist_normal)),"\n")
+    p_gamma = []
+    gamma_gen = np.random.gamma(10,3, 100)
+    stats, p = kstest(gamma_gen, dist_gamma)
+    p_gamma.append(p)
 
-print("Distribucion pascal:", dist_pascal)
-print("Distribucion pascal normalizada:", normalizar(dist_pascal))
-print("TEST CHI CUADRADO PARA PASCAL:", chisq_test(normalizar(dist_pascal)),"\n")
+    p_normal = []
+    normal_gen = np.random.normal(2.5, 25, 100)
+    stats, p = kstest(normal_gen, dist_normal)
+    p_normal.append(p)
 
-print("Distribucion binomial:", dist_binomial)
-print("Distribucion binomial normalizada:", normalizar(dist_binomial))
-print("TEST CHI CUADRADO PARA BINOMIAL:", chisq_test(normalizar(dist_binomial)),"\n")
+    p_pascal = []
+    pascal_gen = np.random.negative_binomial(4, 0.8, 100)
+    stats, p = kstest(pascal_gen, dist_pascal)
+    p_pascal.append(p)
 
-print("Distribucion hipergeometrica:", dist_hipergeometrica)
-print("Distribucion hipergeometrica normalizada:", normalizar(dist_hipergeometrica))
-print("TEST CHI CUADRADO PARA HIPERGEOMETRICA:", chisq_test(normalizar(dist_hipergeometrica)),"\n")
+    p_binomial = []
+    binomial_gen = np.random.binomial(100, 0.5, 100)
+    stats, p = kstest(binomial_gen, dist_binomial)
+    p_binomial.append(p)
 
-print("Distribucion poisson:", dist_poisson)
-print("Distribucion poisson normalizada:", normalizar(dist_poisson))
-print("TEST CHI CUADRADO PARA POISSON:", chisq_test(normalizar(dist_poisson)),"\n")
+    p_hipergeometrica = []
+    hipergeometrica_gen = np.random.hypergeometric(50,50,100,100)
+    stats, p = kstest(hipergeometrica_gen, dist_hipergeometrica)
+    p_hipergeometrica.append(p)
+
+    p_poisson= []
+    poisson_gen = np.random.poisson(10, 100)
+    stats,p =kstest(poisson_gen,dist_poisson)
+    p_poisson.append(p)
+
+for j in range(10):
+    dist_empirica = empirica(em_sample)
+    empirica_gen = np.random.normal(0,1,100)
+    normalized_empirica = dist_empirica/np.linalg.norm(dist_empirica)
+    stats,p =kstest(empirica_gen,normalized_empirica)
+    print(p)
+
+
